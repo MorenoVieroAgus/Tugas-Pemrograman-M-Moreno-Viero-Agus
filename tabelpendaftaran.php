26eebe7df311e0ef75b9579b0a9e908eac1062f5
MorenoVieroAgus <reno39299@gmail.com>
2024-12-17 08:25:31 +0700

    Add files via upload

---
+++ tabelpendaftaran.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <table class="table">
+  <table class="table table-dark table-striped">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Id</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Nim</th>
+      <th scope="col">Jk</th>
+      <th scope="col">Email</th>
+      <th scope="col">Password</th>
+      <th scope="col">Alamat</th>
+      <th scope="col">Tanggal lahir</th>
+      <th scope="col">Tempat lahir</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>0046</td>
+      <td>M Moreno Viero Agus</td>
+      <td>701230251</td>
+      <td>Laki Laki</td>
+      <td>morenoganteng1@gmail.com</td>
+      <td>heruburuk</td>
+      <td>Paal Merah</td>
+      <td>06 Juni 2005</td>
+      <td>Jambi</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+  </body>
+</html>
+<form>
